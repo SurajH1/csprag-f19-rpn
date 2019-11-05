9e602e4ff3cad9a5833e4acb4cf3943f9000d6b3
sharjani <sharjani@umich.edu>
2019-11-05 08:29:35 -0500

    Was able to get the working python files from the class git repository

--- rpn.py
+++ rpn.py
@@ -1,25 +1,36 @@
 #!/usr/bin/env python3
-def calculate(arg):
-        stack = list()
-        for token in arg.split():
-                print(token)
-                if token == '+':
-                    arg1 = stack.pop()
-                    arg2 = stack.pop()
-                    result = arg1 + arg2
-                    stack.append(result)
-                elif token == '-':
-                    arg2 = stack.pop()
-                    arg1 = stack.pop()
-                    result = arg1 - arg2
-                    stack.append(result)
-                else:
-                    stack.append(int(token))
-                print(stack)
-        return stack.pop()
+
+import operator
+
+
+operators = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
+}
+
+def calculate(myarg):
+    stack = list()
+    for token in myarg.split():
+        try:
+            token = int(token)
+            stack.append(token)
+        except ValueError:
+            function = operators[token]
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = function(arg1, arg2)
+            stack.append(result)
+        print(stack)
+    if len(stack) != 1:
+        raise TypeError("Too many parameters")
+    return stack.pop()
+
 def main():
-	while True:
-		calculate(input('rpn calc> '))
+    while True:
+        result = calculate(input("rpn calc> "))
+        print("Result: ", result)
 
 if __name__ == '__main__':
-	main()
+    main()
